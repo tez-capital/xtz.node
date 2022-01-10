3659488cdd7fb0e282bb0efff8836d441cb49c3c
V <cryi@tutanota.com>
2022-01-10 22:01:56 +0100

    fixed import-key --force

--- src/__xtz/import_key.lua
+++ src/__xtz/import_key.lua
@@ -1,9 +1,18 @@
 local _args = table.pack(...)
 
 ami_assert(#_args > 0, "Please provide baker address...")
+local _importArgs = { "import", "secret", "key", "baker", am.app.get_model("REMOTE_SIGNER_ADDR", "tcp://127.0.0.1:2222/") .. _args[1] }
+
+for _, v in ipairs(_args) do
+	if string.trim(v) == "-f" or string.trim(v) == "--force" then
+		table.insert(_importArgs, "--force")
+		break
+	end
+end
+
 
 local _user = am.app.get("user")
-local _proc = proc.spawn("bin/client", { "import", "secret", "key", "baker", am.app.get_model("REMOTE_SIGNER_ADDR", "tcp://127.0.0.1:2222/") .. _args[1] }, {
+local _proc = proc.spawn("bin/client", _importArgs, {
 	stdio = "inherit",
 	wait = true,
 	env = { HOME = path.combine(os.cwd(), "data") }
