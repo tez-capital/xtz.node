937a1afdbb26877f01949a97b9031a3f1818e88e
V <cryi@tutanota.com>
2022-04-05 16:15:28 +0200

    added support for 'log -e'

--- src/ami.lua
+++ src/ami.lua
@@ -128,6 +128,11 @@ return {
                     aliases = {"f"},
                     description = "Keeps printing the log continuously.",
                     type = "boolean"
+                },
+                ["end"] = {
+                    aliases = {"e"},
+                    description = "Jumps to the end of the log.",
+                    type = "boolean"
                 }
             },
             type = "no-command",
