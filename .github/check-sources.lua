1001b27fad0cc656205464ce3414529026f5a966
v <cryi@tutanota.com>
2024-06-09 18:23:09 +0200

    improved check sources

--- .github/check-sources.lua
+++ .github/check-sources.lua
@@ -4,6 +4,7 @@ local sourcesHjson = fs.read_file("src/__xtz/sources.hjson")
 local sources = hjson.parse(sourcesHjson)
 
 local sourceUrls = {}
+local sourceIds = {}
 
 for _, platformSources in pairs(sources) do
 	for _, sourceUrl in pairs(platformSources) do
@@ -11,6 +12,17 @@ for _, platformSources in pairs(sources) do
 			error("Duplicate source url: " .. sourceUrl)
 		end
 		table.insert(sourceUrls, sourceUrl)
+
+		-- https://gitlab.com/tezos/tezos/-/package_files/130339715/download
+		-- extract id from url
+		local sourceId = sourceUrl:match("package_files/(%d+)/download")
+		if not sourceId then
+			error("Invalid source url: " .. sourceUrl)
+		end
+		if table.includes(sourceIds, sourceId) then
+			error("Duplicate source id: " .. sourceId)
+		end
+		table.insert(sourceIds, sourceId)
 	end
 end
 
