fa181d546c5e05280940c6e9fb8c0f6dd42cc3fd
v <cryi@tutanota.com>
2025-06-15 20:52:16 +0200

    updated sources

--- src/__xtz/update-sources.lua
+++ src/__xtz/update-sources.lua
@@ -1,18 +1,19 @@
 -- SOURCE: https://gitlab.com/tezos/tezos/-/releases
--- eli src/__xtz/update-sources.lua https://gitlab.com/tezos/tezos/-/packages/25835249 PsParisC
+-- eli src/__xtz/update-sources.lua https://gitlab.com/tezos/tezos/-/packages/25835249 https://github.com/tez-capital/tezos-macos-pipeline/releases/tag/octez-v22.1-2025-06-11_20-14 PsRiotum
 
 local hjson = require "hjson"
 local args = table.pack(...)
-if #args < 2 then
-	print("Usage: update-sources <source-url> [protocols]")
+if #args < 3 then
+	print("Usage: update-sources <source-url> <macos-source-url> [protocols]")
 	return
 end
 
 local source = args[1]
-local protocol = args[2]
+local macos_source = args[2]
+local protocol = args[3]
 local protocol_next
 if #args > 2 then
-	protocol_next = args[3]
+	protocol_next = args[4]
 end
 
 --- extract package id from url source - https://gitlab.com/tezos/tezos/-/packages/25835249
@@ -31,39 +32,71 @@ for platform, sources in pairs(current_sources) do
 	local new_sources = {}
 	-- extract arch from linux-x86_64
 	local arch = platform:match("linux%-(.*)")
-	for source_id, source_url in pairs(sources) do
-		if source_id == "prism" then
-			new_sources[source_id] = source_url
-			goto CONTINUE
-		end
-
-		-- build asset id => <arch>-octez-<source_id>
-		local asset_ids = { [source_id] = arch .. "-octez-" .. source_id }
-		if source_id:match("baker") or source_id:match("accuser") then
-			asset_ids[source_id] = arch .. "-octez-" .. source_id .. "-" .. protocol
-			if protocol_next then
-				asset_ids[source_id .. "-next"] = arch .. "-octez-" .. source_id .. "-" .. protocol_next
+	if arch then -- linux
+		for source_id, source_url in pairs(sources) do
+			if source_id == "prism" then
+				new_sources[source_id] = source_url
+				goto CONTINUE
 			end
-		end
 
-		for asset_id, asset_name in pairs(asset_ids) do
-			-- lookup file id
-			for _, file in ipairs(files) do
-				if file.file_name == asset_name then
-					-- update source url
-					-- https://gitlab.com/tezos/tezos/-/package_files/<id>/download
-					new_sources[asset_id] = "https://gitlab.com/tezos/tezos/-/package_files/" .. file.id .. "/download"
-					break
+			-- build asset id => <arch>-octez-<source_id>
+			local asset_ids = { [source_id] = arch .. "-octez-" .. source_id }
+			if source_id:match("baker") or source_id:match("accuser") then
+				asset_ids[source_id] = arch .. "-octez-" .. source_id .. "-" .. protocol
+				if protocol_next then
+					asset_ids[source_id .. "-next"] = arch .. "-octez-" .. source_id .. "-" .. protocol_next
 				end
 			end
+
+			for asset_id, asset_name in pairs(asset_ids) do
+				-- lookup file id
+				for _, file in ipairs(files) do
+					if file.file_name == asset_name then
+						-- update source url
+						-- https://gitlab.com/tezos/tezos/-/package_files/<id>/download
+						new_sources[asset_id] = "https://gitlab.com/tezos/tezos/-/package_files/" ..
+						file.id .. "/download"
+						break
+					end
+				end
+			end
+			::CONTINUE::
 		end
-		::CONTINUE::
+	end
+	local arch = platform:match("darwin%-(.*)")
+	if arch then -- macos
+		for source_id, source_url in pairs(sources) do
+			if source_id == "prism" then
+				new_sources[source_id] = source_url
+				goto CONTINUE
+			end
+
+			-- extract tag from macos_source url
+			-- e.g. https://github.com/tez-capital/tezos-macos-pipeline/releases/tag/octez-v22.1-2025-06-11_20-14
+			local tag = macos_source:match("/tag/([^/]+)")
+			assert(tag, "Invalid macos source url")
+
+			-- build asset id => <arch>-octez-<source_id>
+			local asset_ids = { [source_id] = "octez-" .. source_id }
+			for asset_id, asset_name in pairs(asset_ids) do
+				-- lookup file id
+				for _, file in ipairs(files) do
+					-- update source url
+					-- https://github.com/tez-capital/tezos-macos-pipeline/releases/download/octez-v22.0-2025-06-04_15-52/octez-dal-node
+					new_sources[asset_id] = "https://github.com/tez-capital/tezos-macos-pipeline/releases/download/" ..
+						tag .. "/" ..
+						asset_name
+				end
+			end
+			::CONTINUE::
+		end
 	end
 	current_sources[platform] = new_sources
 end
 
-local new_content = "// SOURCE: " .. source .. " \n"
-new_content = new_content .. "// PROTOCOLS: " .. string.join(", ", protocol, protocol_next) .. "\n"
+local new_content = "// SOURCE: " .. source .. " \n" ..
+	"// macOS SOURCE: " .. macos_source .. "\n" ..
+	"// PROTOCOLS: " .. string.join(", ", protocol, protocol_next) .. "\n"
 new_content = new_content .. hjson.stringify(current_sources, { separator = true, sort_keys = true })
 
 fs.write_file("src/__xtz/sources.hjson", new_content)
